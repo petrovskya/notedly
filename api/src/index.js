45850395d2758a68972acf1306f86af208f0f843
petrovskya <petrovska.ya@icloud.com>
2024-02-20 19:29:17 +0300

    feat: setup a GrapghQL server with Apollo)

---
+++ api/src/index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
+//Запускаем сервер на порте, указанном в файле .env, или на порте 4000
+const port = process.env.PORT || 4000;
+
+const notes = [
+  { id: '1', content: 'This is a note', author: 'Adam Scott' },
+  { id: '2', content: 'This is another note', author: 'Harlow Everly' },
+  { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' },
+];
+
+//Построение схемы с использованием языка схем GraphQL
+const typeDefs = gql`
+  type Query {
+    hello: String!
+    notes: [Note!]!
+    note(id: ID!): Note!
+  }
+
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
+
+  type Mutation {
+    newNote(content: String!): Note!
+  }
+`;
+
+//Предоставляем функцию разрешения для полей схемы
+const resolvers = {
+  Query: {
+    hello: () => 'Hello World!',
+    notes: () => notes,
+    note: (parent, args) => notes.find((note) => note.id === args.id),
+  },
+
+  Mutation: {
+    newNote: (parent, args) => {
+      const noteValue = {
+        id: String(notes.length + 1),
+        content: args.content,
+        author: 'Adam Scott',
+      };
+
+      notes.push(noteValue);
+
+      return noteValue;
+    },
+  },
+};
+
+const app = express();
+
+//Настройка Apollo Server
+const server = new ApolloServer({ typeDefs, resolvers });
+
+//Применяем промежуточное ПО Apollo GraphQl и указываем путь к /api
+server.applyMiddleware({ app, path: '/api' });
+
+app.listen({ port }, () =>
+  console.log(
+    `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`,
+  ),
+);
